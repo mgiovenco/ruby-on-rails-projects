da017f24cf05e5fddf45cea5a47686eab09c3e0e
mgiovenco <michael.giovenco@icrossing.com>
2014-04-11 11:34:40 -0500

    Adding hello world application and basic crud blog application

---
+++ hello/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hello::Application.config.secret_key_base = 'f831be28afb40b1e75719b8be9533d057b74f69cd6f8623d862260d87b881eab01f973bec38f77e468e1d14a0b299f1170c658a67c3f62462e98f849cd696a4e'
